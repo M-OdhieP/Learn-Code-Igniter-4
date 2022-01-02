6fbe5ec88359c61634bc03383ee22dd274d9ee45
M OdhieP <tiotio179@gmail.com>
2022-01-03 03:03:15 +0700

    buat file coba.php

---
+++ app/Controllers/Coba.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+class Coba extends BaseController
+{
+    public function index()
+    {
+       echo 'ini controler coba method index';
+    }
+    public function about()
+    {
+       echo 'Halo nama saya odhie';
+    }
+}
